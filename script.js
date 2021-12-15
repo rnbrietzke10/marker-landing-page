f05b3a7abace127c3a85b1ec117f3e32c85935f5
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-15 11:58:34 -0600

    implement hover state and focus for button and input. Started variables in js file

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const submitBtn = document.querySelector('.btn');
+const error = document.getElementById('error');
