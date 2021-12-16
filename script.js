1766cd000f950532b18cb55a81d6d23266b3512f
Rebecca Brietzke <rnbrietzke@outlook.com>
2021-12-15 18:33:15 -0600

    add error message when no email is entered and button is clicked

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ const form = document.getElementById('form');
 const email = document.getElementById('email');
 const submitBtn = document.querySelector('.btn');
 const error = document.getElementById('error');
+
+const resetError = () => {
+  // clear error text
+  let footer = document.querySelector('footer');
+  let throwawayNode = footer.removeChild(error);
+};
+
+submitBtn.addEventListener(
+  'click',
+  function (event) {
+    event.preventDefault();
+
+    if (email.value.length === 0) {
+      var textnode = document.createTextNode('Oops! Please add your email'); // Create a text node
+      error.appendChild(textnode);
+    }
+  },
+  { once: true }
+);
+
+email.addEventListener('input', resetError);
